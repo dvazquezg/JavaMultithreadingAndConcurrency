8f14afefef5527a70c1125a484c468184f649ea0
Daniel Vazquez Guevara <dany160990@gmail.com>
2025-10-01 18:24:36 -0700

    Section 4 throughput example completed

---
+++ src/threads/section4/example2/ThroughputHttpServer.java
@@ -0,0 +1,30 @@
+package threads.section4.example2;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/** Optimizing for Throughput Part 2 - HTTP server + Jmeter */
+public class ThroughputHttpServer {
+  private static final String INPUT_FILE = "./resources/war_and_peace.txt";
+  private static final int NUMBER_OF_THREADS = 8;
+
+  public static void main(String[] args) throws IOException {
+    String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+    startServer(text);
+  }
+
+  public static void startServer(String text) throws IOException {
+    HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+    server.createContext("/search", new WordCountHandler(text));
+    // create executor thread pool
+    Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    server.setExecutor(executor);
+    server.start();
+  }
+}
